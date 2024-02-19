b99ad061da1d8e7c042271453f85672f2c44fe1e
Yatin2425 <yatintyagi2425@gmail.com>
2024-02-19 20:06:38 +0530

    post and view feature

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ app.get("/", (req, res) => {
     res.render("login.ejs")
 })
 app.post("/", (req, res) => {
+    res.redirect("/handle-login");
+})
+app.post("/handle-login", (req, res) => {
     
     console.log("Email:", req.body.InputEmail); // Log received email
     console.log("Username:", req.body.InputUsername); // Log received username
+    res.redirect("/homepage");
 
     res.render("login.ejs" , {
         email : req.body.InputEmail,
@@ -28,10 +32,31 @@ app.post("/", (req, res) => {
    
     );
 });
+app.post("/handle-post", (req, res) => {
+    const title = req.body.blogTitle;
+    const content = req.body.blogContent;
+    console.log("Title:", title);
+    console.log("Content:", content);
+
+    res.render("view.ejs", {
+        Title: title,
+        Content: content
+    });
+});
+
 
 app.get("/homepage", (req, res) => {
     res.render("homepage.ejs")
 })
-app.post("/handle-login", (req, res) => {
-    res.redirect("homepage");  
-  });
+
+
+
+app.get("/post" , (req, res) => {
+    res.render("post.ejs")
+
+});
+
+app.get("/view", (req, res) => {
+    res.render("view.ejs")
+
+});
